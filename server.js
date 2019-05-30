daf2561001c80bf1042618fa441b8ca3ed5aa3b3
rithesht <ritheshkdr@gmail.com>
2019-05-30 16:03:54 +0530

    switch case to identify intent &  request api structure

--- server.js
+++ server.js
@@ -11,53 +11,35 @@ app.use(bodyParser.json());
 
 // To give out some informations on the current project
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+    response.sendFile(__dirname + '/views/index.html');
 });
 
 // the DialogFlow fulfillment endpoint
 app.post('/webhook/', function(request, response) {
-  const dialogflowRequest = request.body;
-  // An intent's action serves as a mapping mechanism between your intent and a function living in your app.
-   const action = dialogflowRequest.queryResult.action;
+    const dialogflowRequest = request.body;
+    // An intent's action serves as a mapping mechanism between your intent and a function living in your app.
+    const action = dialogflowRequest.queryResult.action;
+    const intent = dialogflowRequest.queryResult.intent.displayName
 
+    switch (intent) {
+        case "Default Welcome Intent":
+            return response.json({ fulfillmentText: `webhook says this is ${intent} intent ` });
+            break;
+        case "static":
+            response.json({ fulfillmentText: `webhook says this is ${intent} intent ` });
+            break;
+        case "total student count":
+            response.json({ fulfillmentText: `webhook says this is ${intent} intent ` });
+            break;
+    }
 
-   if ( action === "totalStudent.action") {
-    return response.json({fulfillmentText:`The total strength is ${membersCount}`});
-   }
-  console.log(action);
-  // these 3 variables could come from your intent's parameters !
-  // const tabName = 'schoolDemo1';
-  // const startCell = 'A2';
-  // const endCell = 'A3';
-  
-  // return googleSpreadsheet.getDataFromSpreadsheet(tabName, startCell, endCell)
-  //   .then((results) => {
-  //     response.json(
-  //       dialogflow.convertFormat(results)
-  //     );
-  //   }).catch((error) => {
-  //     throw new Error(error);
-  //   });
-  // speech = "Hello there, this reply is from the webhook !! "
-  //   string = "You are awesome !!"
-  //   Message ="this is the message"
-
-  //   my_result =  {
-
-  //   "fulfillmentText": string,
-  //    "source": string
-  //   }
-
-  //   res = json.dumps(my_result, indent=4)
-
-  //   r = make_response(res)
-
-  //   r.headers['Content-Type'] = 'application/json'
-  //   return r
-  return response.json({fulfillmentText:"This is from webhook recheck"});
+    if (action === "totalStudent.action") {
+        return response.json({ fulfillmentText: `The total strength is ${membersCount}` });
+    }
+    return response.json({ fulfillmentText: "This is from webhook recheck" });
 });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+    console.log('Your app is listening on port ' + listener.address().port);
 });
